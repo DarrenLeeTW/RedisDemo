991307ac15eeb2c884c977edbf6aaf220859bf77
Darren <jinewen@gmail.com>
2023-02-22 18:02:24 +0800

    加入專案檔案。

---
+++ RedisDemo/Program.cs
@@ -0,0 +1,9 @@
+﻿using RedisDemo;
+using StackExchange.Redis;
+using System.Reflection;
+
+    var connection = new RedisConnection();
+    // var database = connection.Connect("localhost:6379");
+    var config = ConfigurationOptions.Parse("127.0.0.1:6379");
+    var database = connection.Connect(config.ToString());
+    database.Set("dto", "123");
---
+++ RedisDemo/RedisConnection.cs
@@ -0,0 +1,33 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisDemo
+{
+    internal class RedisConnection
+    {
+        private static ConcurrentDictionary<string, Lazy<ConnectionMultiplexer>> s_connectionPool = new();
+
+        public IDatabase Connect(string setting = "localhost")
+        {
+            var connMultiplexer = s_connectionPool.GetOrAdd(setting,
+                new Lazy<ConnectionMultiplexer>(() => ConnectionMultiplexer.Connect(setting)));
+
+            return connMultiplexer.Value.GetDatabase();
+        }
+
+        public IDatabase GetDatabase(string setting = "localhost")
+        {
+            if (s_connectionPool.TryGetValue(setting, out var connMultiplexer))
+            {
+                return connMultiplexer.Value.GetDatabase();
+            }
+
+            return default;
+        }
+    }
+}
